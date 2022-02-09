4fe81987dab588ad05227e5da5fd48859ff2b78b
Missyesterday <hyf1204@icloud.com>
2022-02-09 17:03:46 +0800

    hello.java

--- src/main/java/com/example/gitstudy/Hello.java
+++ src/main/java/com/example/gitstudy/Hello.java
@@ -3,5 +3,6 @@ package com.example.gitstudy;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("nihao ");
     }
 }
